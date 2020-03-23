b8f2670ebf7f8cfb571078b4d503f29ede3ef13f
IgorVNC <57942649+IgorVNC@users.noreply.github.com>
2020-03-23 14:54:12 +0200

    iOS uiwebview cleanup (#41)
    * get rid of FirebaseAuth deps
    * get rid of Crashlitics deps

--- scripts/after_plugin_add.js
+++ scripts/after_plugin_add.js
@@ -17,8 +17,8 @@ module.exports = function(context) {
     // Add a build phase which runs a shell script that executes the Crashlytics
     // run command line tool which uploads the debug symbols at build time.
     if (platforms.indexOf("ios") !== -1) {
-        var xcodeProjectPath = utilities.getXcodeProjectPath(context);
-        iosHelper.removeShellScriptBuildPhase(context, xcodeProjectPath);
-        iosHelper.addShellScriptBuildPhase(context, xcodeProjectPath);
+        // var xcodeProjectPath = utilities.getXcodeProjectPath(context);
+        // iosHelper.removeShellScriptBuildPhase(context, xcodeProjectPath);
+        // iosHelper.addShellScriptBuildPhase(context, xcodeProjectPath);
     }
 };
