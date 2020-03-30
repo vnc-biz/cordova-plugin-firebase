bfe00ae83aa52c8b2fdfc7350c2754c80a5648a4
ValVNC <valentyn.tereshchenko@vnc.biz>
2020-03-30 14:21:59 +0300

    - enable FCM events logging only for VNCtalk

--- src/android/utils/FcmLoggerUtils.java
+++ src/android/utils/FcmLoggerUtils.java
@@ -1,6 +1,7 @@
 package org.apache.cordova.firebase.utils;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.apache.cordova.firebase.actions.LogFCMPluginAction;
 
@@ -8,6 +9,11 @@ import java.util.concurrent.Executors;
 
 public class FcmLoggerUtils {
     public static void logFcmReceived(Context context, String msgId){
+        String appId = getApplicationContext().getPackageName();
+        Log.i("FirebasePlugin.FcmLoggerUtils", "app ID: " + appId);
+
+        if (!appId.equals("biz.vnc.vnctalk")) return;
+
         Executors.newSingleThreadExecutor().submit(new LogFCMPluginAction(context, msgId));
     }
 }
