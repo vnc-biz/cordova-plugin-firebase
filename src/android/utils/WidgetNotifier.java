18e79f84ecd0585f6262692d175030836847d07f
ValVNC <valentyn.tereshchenko@vnc.biz>
2020-11-27 12:21:39 +0200

    feat: add update widget after receiving new fcm or after update appointment from notification;

--- src/android/utils/WidgetNotifier.java
+++ src/android/utils/WidgetNotifier.java
@@ -12,15 +12,38 @@ public class WidgetNotifier {
             AppWidgetManager manager = AppWidgetManager.getInstance(context);
             final int[] appWidgetIds = manager.getAppWidgetIds(new ComponentName(context, "biz.vnc.mailwidget.WidgetProvider"));
 
-            refreshListsForWidgets(context, manager, appWidgetIds);
+            refreshListsForMailWidgets(context, manager, appWidgetIds);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void refreshListsForWidgets(Context context, AppWidgetManager manager, int[] appWidgetIds) {
+    private static void refreshListsForMailWidgets(Context context, AppWidgetManager manager, int[] appWidgetIds) {
         if (appWidgetIds.length == 0) return;
 
         manager.notifyAppWidgetViewDataChanged(appWidgetIds, context.getResources().getIdentifier("messages_list", "id", context.getPackageName()));
+    }
+    
+    public static void updateCalendarWidgets(Context context) {
+        try {
+            AppWidgetManager manager = AppWidgetManager.getInstance(context);
+            
+            final int[] appMonthWidgetIds = manager.getAppWidgetIds(new ComponentName(context, "biz.vnc.calendarwidget.WidgetProviderMonth"));
+            refreshListsForCalendarWidgets(context, manager, appMonthWidgetIds);
+
+            final int[] appMonthListWidgetIds = manager.getAppWidgetIds(new ComponentName(context, "biz.vnc.calendarwidget.WidgetProviderMonthList"));
+            refreshListsForCalendarWidgets(context, manager, appMonthListWidgetIds);
+
+            final int[] appWeekWidgetIds = manager.getAppWidgetIds(new ComponentName(context, "biz.vnc.calendarwidget.WidgetProviderWeek"));
+            refreshListsForCalendarWidgets(context, manager, appWeekWidgetIds);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void refreshListsForCalendarWidgets(Context context, AppWidgetManager manager, int[] appWidgetIds) {
+        if (appWidgetIds.length == 0) return;
+
+        manager.notifyAppWidgetViewDataChanged(appWidgetIds, context.getResources().getIdentifier("appointments_list", "id", context.getPackageName()));
     }
 }
